9ce0750094987559f1a8a8474ae787b800c4ae71
Joshua Ogden <joshua.ogden@magairports.com>
2020-02-27 13:55:28 +0000

    Testing Background Video

--- my-app/src/views/homePage.js
+++ my-app/src/views/homePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './homePage.scss';
 import HeroImage from '../assets/images/Lost in Koh Mak.jpg';
 import SecondImage from '../assets/images/Top-Travel-Couples-Lebuzz-Header-1920x1126.jpg';
+import Video from '../assets/video/BackgroundVideo.mp4';
 
 
 class Home extends Component {
@@ -22,8 +23,23 @@ class Home extends Component {
                   </h3>
                 </div>
               </div>
-              <img className="homePage__content--header--backgroundImage" src={SecondImage} alt="A young girl on a moped, exploring a lush green tropical forrest." height="100%" width="100%">
-                </img> 
+              <div className="homePage__content--header">
+                {/* <img className="homePage__content--header--backgroundImage" src={SecondImage} alt="A young girl on a moped, exploring a lush green tropical forrest." height="100%" width="100%">
+                </img>  */}
+
+                <video autoplay loop muted className="test">
+                  <source  src={Video} type="video/mp4" />
+                </video>
+
+                <div className="homePage__content--header--textWrapper">
+                  <h1 className="homePage__content--header--textWrapper--title">
+                    JOSH OGDEN
+                    </h1>
+                  <h3 className="homePage__content--header--textWrapper--subtitle">
+                    Create your own reality
+                  </h3>
+                </div>
+              </div>
             </div>
           </div>
         );
