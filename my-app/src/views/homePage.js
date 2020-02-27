950840da0bbded15ca14be3d23cdf8137a91f648
Joshua Ogden <joshua.ogden@magairports.com>
2020-02-27 15:36:28 +0000

    HomePage Changes

--- my-app/src/views/homePage.js
+++ my-app/src/views/homePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './homePage.scss';
 import HeroImage from '../assets/images/Lost in Koh Mak.jpg';
-// import SecondImage from '../assets/images/Top-Travel-Couples-Lebuzz-Header-1920x1126.jpg';
+import SecondImage from '../assets/images/Top-Travel-Couples-Lebuzz-Header-1920x1126.jpg';
 import Video from '../assets/video/BackgroundVideo.mp4';
 
 
@@ -10,34 +10,51 @@ class Home extends Component {
         return (
           <div className="homePage">
             <div className="homePage__content">
-              <div className="homePage__content--header">  
+              <div className="homePage__content--section">  
                   
                  <video autoPlay muted playsinline loop muted className="test" poster="../assets/images/Lost in Koh Mak.jpg">
                   <source  src={Video} type="video/mp4" />
                 </video>
-                  <div className="homePage__content--header--textWrapper">
-                    <h1 className="homePage__content--header--textWrapper--title">
+                <div className="homePage__content--section--textWrapper">
+                  <h1 className="homePage__content--section--textWrapper--title">
                       JOSH OGDEN
                     </h1>
-                    <h3 className="homePage__content--header--textWrapper--subtitle">
+                  <h3 className="homePage__content--section--textWrapper--subtitle">
                       Create your own reality
                   </h3>
                 </div>
               </div>
-              <div className="homePage__content--header">
+              <div className="homePage__content--section">
                 {/* <img className="homePage__content--header--backgroundImage" src={SecondImage} alt="A young girl on a moped, exploring a lush green tropical forrest." height="100%" width="100%">
                 </img>  */}
 
-                <img className="homePage__content--header--backgroundImage" src={HeroImage} alt="A young lady, exploring a lush green tropical forrest whilst riding on a retro moped." height="100%" width="100%">
+                <img className="homePage__content--section--backgroundImage" src={HeroImage} alt="A young lady, exploring a lush green tropical forrest whilst riding on a retro moped." height="100%" width="100%">
                   </img>
 
-                <div className="homePage__content--header--textWrapper">
-                  <h1 className="homePage__content--header--textWrapper--title">
-                    JOSH OGDEN
-                    </h1>
-                  <h3 className="homePage__content--header--textWrapper--subtitle">
-                    Create your own reality
+                <div className="homePage__content--section--textWrapper">
+                  <h3 className="homePage__content--section--textWrapper--paragraphTitle">
+                    What can I do for you?
                   </h3>
+
+                  <p className="homePage__content--section--textWrapper--paragraph">Hey there, my name is Josh and this is just a breif introduction to myself and my brand new website, which I have recently created. This website is here just to be a bit of a portfolio about myself and a way for possible clients to see all of my best work. I have worked with a few clients before but I mainly create videos for myself and my Social Channels, but I would like to expand my knowledge and portfolio by working with other companies and brands to help promote thier buisness needs. </p>
+                  <p className="homePage__content--section--textWrapper--paragraph">Feel free to browse my site and my social channels to see more of the work I do and if you like what you see and would like to colaberate or work with me. Feel free to send me a message from the <a href="/contactMe">Contact Me</a> page. I am looking forward to working with lots of new people! </p>
+                </div>
+              </div>
+
+              <div className="homePage__content--section">
+                {/* <img className="homePage__content--header--backgroundImage" src={SecondImage} alt="A young girl on a moped, exploring a lush green tropical forrest." height="100%" width="100%">
+                </img>  */}
+
+                <img className="homePage__content--section--backgroundImage" src={SecondImage} alt="A young lady, exploring a lush green tropical forrest whilst riding on a retro moped." height="100%" width="100%">
+                </img>
+
+                <div className="homePage__content--section--textWrapper">
+                  <h3 className="homePage__content--section--textWrapper--paragraphTitle">
+                    Testemonials
+                  </h3>
+
+                  <p className="homePage__content--section--textWrapper--paragraph">Hey there, my name is Josh and this is just a breif introduction to myself and my brand new website, which I have recently created. This website is here just to be a bit of a portfolio about myself and a way for possible clients to see all of my best work. I have worked with a few clients before but I mainly create videos for myself and my Social Channels, but I would like to expand my knowledge and portfolio by working with other companies and brands to help promote thier buisness needs. </p>
+                  
                 </div>
               </div>
             </div>
